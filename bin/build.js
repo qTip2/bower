a16b7f9ed6dbb2d4cbec9ab213c01e60cb5aff0d
Craig Michael Thompson <craig@craigsworks.com>
2013-09-11 10:51:55 +0100

    Add bower.json last so it's not cleaned by grunt. Add missing bower.json

--- bin/build.js
+++ bin/build.js
@@ -7,35 +7,40 @@ var fs = require('fs'),
 	parent = path.join(__dirname, '..'),
 	relative = path.relative(path.join(parent, '..'), parent);
 
-// Output to bower.json
-fs.writeFile('bower.json', JSON.stringify({
-	"name": json.name.toLowerCase(),
-	"description": json.description,
-	"version": json.version,
-	"homepage": "http://qtip2.com",
-	"location": "https://github.com/arrayjam/bower-qtip2/",
-	"repository": json.repository,
-	"authors": [ json.author ],
-	"license": json.licenses.map(function(license) { return license.type }),
-	"keywords": json.keywords,
-	"main": [
-		"./jquery.qtip.js",
-		"./basic/jquery.qtip.js"
-	],
-	"ignore": [
-		"bin"
-	]
-}, null, 4));
+function exec(cmd, callback) {
+	cp.exec(cmd, {}, function(error, stdout, stderr) {
+		if (error !== null) { return console.log('exec error: ' + error); }
+		callback(error, stdout, stderr);
+	});
+}
 
 // Copy files from grunt-ed output
-cp.exec('grunt --dist="'+relative+'" --stable', {}, function(error, stdout, stderr) {
-	if (error !== null) { return console.log('exec error: ' + error); }
+exec('grunt --dist="'+relative+'" --stable', function() {
 
 	// Also generate basic files
-	cp.exec('grunt basic --dist="'+relative+'" --stable', {}, function(error, stdout, stderr) {
-		if (error !== null) { return console.log('exec error: ' + error); }
+	exec('grunt basic --dist="'+relative+'" --stable', function() {
 
 		// Add the files to the git repo
 		cp.exec('git add *');
+
+		// Output to bower.json
+		fs.writeFile('bower.json', JSON.stringify({
+			"name": json.name.toLowerCase(),
+			"description": json.description,
+			"version": json.version,
+			"homepage": "http://qtip2.com",
+			"location": "https://github.com/arrayjam/bower-qtip2/",
+			"repository": json.repository,
+			"authors": [ json.author ],
+			"license": json.licenses.map(function(license) { return license.type }),
+			"keywords": json.keywords,
+			"main": [
+				"./jquery.qtip.js",
+				"./basic/jquery.qtip.js"
+			],
+			"ignore": [
+				"bin"
+			]
+		}, null, 4));
 	});
 });
